c2316fe84297578b08da1973da90f3231455b5a4
MkArs <arsen_mkhitaryan_2000@mail.ru>
2022-09-05 22:21:02 +0300

    Sax solved

--- src/main/java/com/mkhitaryan/test/sax/Test.java
+++ src/main/java/com/mkhitaryan/test/sax/Test.java
@@ -2,53 +2,19 @@ package com.mkhitaryan.test.sax;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class Test {
     public void begin(){
         String url = "https://www.cbr.ru/scripts/XML_daily.asp";
-        URL obj = null;
-
-        try {
-            obj = new URL(url);
-
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-
-        HttpURLConnection connection = null;
-
-        try {
-            connection = (HttpURLConnection) obj.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-
-            in.close();
-
             //SAX парсер
-            try {
-                SAXParserFactory factory = SAXParserFactory.newInstance();
-                SAXParser parser = factory.newSAXParser();
-                TestSAXHandler handler = new TestSAXHandler();
-                parser.parse(response.toString(), handler);
-                System.out.println("SAX parser result:\n" + handler.getResult());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            TestSAXHandler handler = new TestSAXHandler();
+            parser.parse(url, handler);
+            System.out.println("SAX parser result:\n" + handler.getResult());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
