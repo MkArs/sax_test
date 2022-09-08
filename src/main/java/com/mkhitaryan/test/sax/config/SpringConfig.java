d5f9005fc65a95e3b3284daaed8a2adbae53e95d
MkArs <arsen_mkhitaryan_2000@mail.ru>
2022-09-08 18:54:56 +0300

    Adding currencies to the DB

--- src/main/java/com/mkhitaryan/test/sax/config/SpringConfig.java
+++ src/main/java/com/mkhitaryan/test/sax/config/SpringConfig.java
@@ -68,7 +68,7 @@ public class SpringConfig implements WebMvcConfigurer {
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432/converter_db");
         dataSource.setUsername("postgres");
-        dataSource.setPassword("postgress");
+        dataSource.setPassword("postgres");
 
         return dataSource;
     }
@@ -83,8 +83,9 @@ public class SpringConfig implements WebMvcConfigurer {
         properties.put(AvailableSettings.DIALECT, env.getRequiredProperty("hibernate.dialect"));
         properties.put(AvailableSettings.SHOW_SQL, env.getRequiredProperty("hibernate.show_sql"));
         properties.put(AvailableSettings.STATEMENT_BATCH_SIZE, env.getRequiredProperty("hibernate.batch.size"));
-        properties.put(AvailableSettings.HBM2DDL_AUTO, env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put(AvailableSettings.HBM2DDL_AUTO, env.getRequiredProperty("hibernate.hbm2ddl.auto")); // ?????
         properties.put(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, env.getRequiredProperty("hibernate.current.session.context.class"));
+        // properties.put(AvailableSettings.SESSION_FACTORY_NAME, env.getRequiredProperty("sessionFactory"));
         return properties;
     }
 
