42e998013644f275169f19a3c92206cc3e994b02
MkArs <arsen_mkhitaryan_2000@mail.ru>
2022-09-09 18:02:05 +0300

    Adding currencies to the DB

--- src/main/java/com/mkhitaryan/test/sax/config/SpringConfig.java
+++ src/main/java/com/mkhitaryan/test/sax/config/SpringConfig.java
@@ -80,10 +80,10 @@ public class SpringConfig implements WebMvcConfigurer {
 
     private Properties getHibernateProperties() {
         Properties properties = new Properties();
-        properties.put(AvailableSettings.DIALECT, env.getRequiredProperty("hibernate.dialect"));
-        properties.put(AvailableSettings.SHOW_SQL, env.getRequiredProperty("hibernate.show_sql"));
+        properties.put(AvailableSettings.DIALECT, env.getProperty("hibernate.dialect"));
+        properties.put(AvailableSettings.SHOW_SQL, env.getProperty("hibernate.show_sql"));
         // properties.put(AvailableSettings.STATEMENT_BATCH_SIZE, env.getRequiredProperty("hibernate.batch.size"));
-        properties.put(AvailableSettings.HBM2DDL_AUTO, env.getRequiredProperty("hibernate.hbm2ddl.auto")); // ?????
+        properties.put(AvailableSettings.HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto")); // ?????
         // properties.put(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, env.getRequiredProperty("hibernate.current.session.context.class"));
         // properties.put(AvailableSettings.SESSION_FACTORY_NAME, env.getRequiredProperty("sessionFactory"));
         return properties;
