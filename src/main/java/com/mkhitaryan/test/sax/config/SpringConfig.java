bcb09575a2e24d51d3004a6f818f8586616f725d
MkArs <arsen_mkhitaryan_2000@mail.ru>
2022-09-14 23:50:05 +0300

    Working on converter page

--- src/main/java/com/mkhitaryan/test/sax/config/SpringConfig.java
+++ src/main/java/com/mkhitaryan/test/sax/config/SpringConfig.java
@@ -1,7 +1,6 @@
 package com.mkhitaryan.test.sax.config;
 
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
