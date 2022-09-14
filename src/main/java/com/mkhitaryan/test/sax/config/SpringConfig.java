18da180e21bc357b62e55f51bec6dbf1243bbf16
MkArs <arsen_mkhitaryan_2000@mail.ru>
2022-09-14 22:49:11 +0300

    Working on converter page

--- src/main/java/com/mkhitaryan/test/sax/config/SpringConfig.java
+++ src/main/java/com/mkhitaryan/test/sax/config/SpringConfig.java
@@ -1,6 +1,5 @@
 package com.mkhitaryan.test.sax.config;
 
-import org.hibernate.cfg.AvailableSettings;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -17,10 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -47,7 +40,6 @@ public class SpringConfig implements WebMvcConfigurer {
         templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
     }
-
     @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
@@ -65,37 +57,4 @@ public class SpringConfig implements WebMvcConfigurer {
         registry.viewResolver(resolver);
     }
 
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl(env.getProperty("datasource.jdbc.url"));
-//        dataSource.setUsername(env.getProperty("datasource.user"));
-//        dataSource.setPassword(env.getProperty("datasource.password"));
-//
-//        return dataSource;
-//    }
-//
-//    @Bean
-//    public JdbcTemplate jdbcTemplate() {
-//        return new JdbcTemplate(dataSource());
-//    }
-
-//    private Properties getHibernateProperties() {
-//        Properties properties = new Properties();
-//        properties.put(AvailableSettings.DIALECT, env.getProperty("hibernate.dialect"));
-//        properties.put(AvailableSettings.SHOW_SQL, env.getProperty("hibernate.show_sql"));
-//        properties.put(AvailableSettings.HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
-//        return properties;
-//    }
-
-//    @Bean
-//    public LocalSessionFactoryBean getSessionFactory() {
-//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//        sessionFactory.setDataSource(dataSource());
-//        sessionFactory.setPackagesToScan(new String[] { "com.mkhitaryan.test.sax" });
-//        sessionFactory.setHibernateProperties(getHibernateProperties());
-//        return sessionFactory;
-//    }
 }
