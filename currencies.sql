d2eb8385ba78cf1d22cd63c6939bb86380c0bab1
MkArs <arsen_mkhitaryan_2000@mail.ru>
2022-09-17 13:55:17 +0300

    Delete currencies.sql

--- currencies.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.currencies
-(
-    id character varying(7),
-    nominal integer,
-    name character varying(100),
-    value numeric(7,4),
-    numcode character(3),
-    charcode character(3)
-)
