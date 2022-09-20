a8bc1e69e78efd93ebc8d4831462f575a481021b
MkArs <arsen_mkhitaryan_2000@mail.ru>
2022-09-20 14:50:38 +0300

    Login/registration logic

--- src/main/java/com/mkhitaryan/test/sax/controller/MainController.java
+++ src/main/java/com/mkhitaryan/test/sax/controller/MainController.java
@@ -2,11 +2,12 @@ package com.mkhitaryan.test.sax.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String greeting(){
         return "greeting";
     }
